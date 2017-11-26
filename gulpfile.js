7155c5ba2b8f94464d8d0fe6318dfd3440c34a0c
bjlag <vlad.duplin@ya.ru>
2017-11-26 18:19:07 +0300

    Добавлены задачи для оптимизации картинок, работы с кешем, сборке проекта в продакшен

--- gulpfile.js
+++ gulpfile.js
@@ -13,26 +13,33 @@ const watch = require( 'gulp-watch' );
 const gulpif = require( 'gulp-if' );
 const uglify = require( 'gulp-uglify' );
 const miniCss = require( 'gulp-csso' );
+const imagemin = require('gulp-imagemin');
+const imageminJpeg = require('imagemin-jpeg-recompress');
+const imageminPng = require('imagemin-pngquant');
 const runSequence = require( 'run-sequence' );
+const cache = require('gulp-cache');
 
 // НАСТРОЙКИ
 const path = {
     dist: {
         root: './dist',
-        css: './dist/css'
+        css: './dist/css',
+        fonts: './dist/fonts',
+        images: './dist/images'
     },
     src: {
         root: './src',
         html: './src/*.html',
         sass: './src/sass/**/*.+(sass|scss)',
         css: './src/css',
-        assets: './src/assets/**/*'
+        fonts: [
+            './src/fonts/**/*',
+            './src/lib/bootstrap/fonts/**/*'
+        ],
+        images: './src/images/**/*'
     },
-    build: [
-        './src/fonts/**/*',
-        './src/images/**/*',
-        // './src/assets/css/**/*.css',
-        './src/*.html'
+    php: [
+        // './src/include/**/*'
     ],
     watch: [
         './src/сss/*.css',
@@ -42,8 +49,12 @@ const path = {
 };
 
 // ЗАДАЧИ
-gulp.task( 'clean', () => {
+gulp.task( 'clean:dist', () => {
     return del( path.dist.root );
+} );
+
+gulp.task( 'clean:cache', ( done ) => {
+    return cache.clearAll( done );
 } );
 
 gulp.task( 'html', () => {
@@ -82,12 +93,63 @@ gulp.task( 'watch', () => {
     watch( './bower.json', () => gulp.start( 'bower' ) );
 } );
 
-gulp.task( 'build', [ 'clean' ], () => {
+gulp.task( 'build:fonts', () => {
+    return gulp.src( path.src.fonts )
+        .pipe( gulp.dest( path.dist.fonts ) );
+} );
+
+gulp.task( 'build:images', () => {
+    return gulp.src( path.src.images )
+        .pipe( cache(
+            imagemin(
+                [
+                    imagemin.gifsicle( {
+                        interlaced: true
+                    } ),
+                    imagemin.jpegtran( {
+                        progressive: true
+                    } ),
+                    imageminJpeg( {
+                        loops: 5,
+                        min: 75,
+                        max: 80,
+                        quality:'high'
+                    } ),
+                    imagemin.svgo(),
+                    imagemin.optipng( {
+                        optimizationLevel: 3
+                    } ),
+                    imageminPng( {
+                        speed: 5
+                    } )
+                ],
+                {
+                    verbose: true
+                }
+            )
+        ) )
+        .pipe( gulp.dest( path.dist.images ) );
+} );
+
+gulp.task( 'build:php', () => {
+    return gulp.src( path.php )
+        .pipe( gulp.dest( path.dist.root ) );
+} );
+
+gulp.task( 'build:mini', () => {
     return gulp.src( path.src.html )
         .pipe( useref() )
         .pipe( gulpif( '*.js', uglify() ) )
         .pipe( gulpif( '*.css', miniCss( { comments: false } ) ) )
-        .pipe( gulp.dest( path.dist.root ) )
+        .pipe( gulp.dest( path.dist.root ) );
+} );
+
+gulp.task( 'build', () => {
+    runSequence(
+        'clean:dist',
+        [ 'build:fonts', 'build:mini', 'build:other' ],
+        'build:images'
+    );
 } );
 
 gulp.task( 'dev', () => {
